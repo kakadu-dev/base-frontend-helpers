4f4bc34ba51095e77908b0e39da92d6e9d1f5d53
Pavel Sturov <paul.sturov@gmail.com>
2020-04-06 16:45:37 +0300

    fixed babel config

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,25 @@
+const modules      = process.env.ESM ? false : 'commonjs'
+const useESModules = !!process.env.ESM
+
 module.exports = {
     presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    esmodules: true,
-                },
-                corejs: 3,
-                useBuiltIns: 'usage',
+        ['module:metro-react-native-babel-preset'],
+        ['@babel/preset-env', {
+            loose: true,
+            shippedProposals: true,
+            modules,
+            targets: {
+                'ie': 9,
             },
-        ],
+        }],
     ],
-    ignore: ['**/*.native.js'],
     plugins: [
-        [
-            'module-resolver',
-            {
-                'root': ['./src', './'],
-                'alias': {
-                    '@test': './__tests__',
-                },
-            },
+        ['module-resolver', {
+            root: ['./src', './'],
+            alias: { '@test': './__tests__' },
+        },
         ],
-        [
-            '@babel/plugin-proposal-class-properties',
-        ],
+        ['@babel/plugin-proposal-class-properties'],
+        ['@babel/plugin-transform-runtime', { useESModules }],
     ],
 }
